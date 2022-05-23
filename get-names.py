0220f6e1815f492883bc52e4d490dfcbb0c0a262
campus <test@test.com>
2022-05-23 21:35:50 +0000

    removed test.py

--- get-names.py
+++ get-names.py
@@ -2,7 +2,8 @@ from urllib import response
 import requests, json
 
 #BASE = "http://77.237.53.201:8113/"
-BASE = "http://localhost:5001/"
+#BASE = "http://localhost:5001/"
+BASE = "http://172.16.0.10/"
 
 data = {
     "name1": "bill1",
